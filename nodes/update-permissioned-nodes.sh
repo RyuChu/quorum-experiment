a52124a8925e872474b1387b4aa0db28df7ef752
Leo <shspsleo@gmail.com>
2020-02-29 22:12:34 +0800

    Modify NumofNodes

--- nodes/update-permissioned-nodes.sh
+++ nodes/update-permissioned-nodes.sh
@@ -5,7 +5,7 @@ NodesNum=$1
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'PEERS=($(cat ~/node_peers))'
 ARR=()
 
-for i in {1..7}; do
+for i in {1..16}; do
 
   Index=$i-1
   ARR+=($(cat permissioned-nodes.json | jq -r ".[$Index] | sub(\"127.0.0.1\"; \"${PEERS[$Index]}\")"))
